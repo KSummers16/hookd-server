81fdcf27f60ddeba912130884c9a05012845ca31
Kimberly Summers <kasummers16@gmail.com>
2024-05-23 11:26:20 -0500

    updating users

--- hookdapi/models/Customer.py
+++ hookdapi/models/Customer.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     email_address = models.EmailField()
     name = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
+
+    def is_admin(self):
+        return self.user.is_superuser
