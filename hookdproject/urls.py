e4d1d6bde30a86ca5869c5e806130c3ef858e399
Kimberly Summers <kasummers16@gmail.com>
2024-07-03 11:00:54 -0500

    fixing refresh error

--- hookdproject/urls.py
+++ hookdproject/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import TemplateView
+from django.urls import re_path
 from hookdapi.models import *
 from hookdapi.views import *
 
@@ -27,4 +29,8 @@ urlpatterns = [
     path("login", login_user),
     path("api-token-auth", obtain_auth_token),
     path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
+    re_path(r"^.*", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
