654375d4b6146a21b8ed9d13298d9fdedf3185af
Kimberly Summers <kasummers16@gmail.com>
2024-07-22 19:33:11 -0500

    fixed rtssold

--- hookdapi/models/RTSSold.py
+++ hookdapi/models/RTSSold.py
@@ -6,7 +6,11 @@ class RTSSold(models.Model):
     price = models.IntegerField()
     sold_date = models.DateTimeField(auto_now_add=True)
     order = models.ForeignKey("Order", on_delete=models.SET_NULL, null=True)
-    product_type = models.CharField(max_length=50)
+    product_type = models.CharField(
+        max_length=20,
+        default="rtsproduct",
+        editable=False,  # This makes the field uneditable in the admin interface
+    )
 
     def __str__(self):
         return f"{self.name} - Sold on {self.sold_date}"
